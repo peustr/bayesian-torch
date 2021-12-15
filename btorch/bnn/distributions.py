06d73c449284f8b231da93fcc477097566da8da4
Panagiotis Eustratiadis <peustratiadis@gmail.com>
2021-12-15 16:39:26 +0000

    initializing gaussians with small values for mu and rho so exponentials dont explode

--- btorch/bnn/distributions.py
+++ btorch/bnn/distributions.py
@@ -8,8 +8,8 @@ class ParametricGaussian(nn.Module):
     def __init__(self, shape, device=None, dtype=None):
         super().__init__()
         factory_kwargs = {'device': device, 'dtype': dtype}
-        self.mu = nn.Parameter(torch.randn(shape, **factory_kwargs))
-        self.rho = nn.Parameter(torch.randn(shape, **factory_kwargs))
+        self.mu = nn.Parameter(torch.empty(shape, **factory_kwargs).uniform_(-0.2, 0.2))
+        self.rho = nn.Parameter(torch.empty(shape, **factory_kwargs).uniform_(-4., -1.))
 
     @property
     def sigma(self):
--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name='btorch',
-    version='0.2.4',
+    version='0.2.5',
     packages=find_packages(),
 )
