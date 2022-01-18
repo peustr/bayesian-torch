3f173f23c727e6546b26aacaf8745afdf7ec82c0
Panagiotis Eustratiadis <peustratiadis@gmail.com>
2022-01-18 18:07:05 +0000

    removed prior update from kl divergence loss

--- btorch/bnn/loss.py
+++ btorch/bnn/loss.py
@@ -1,10 +1,6 @@
-def KL_divergence(bnn, update_prior=True):
-    kl_divergence = 0.
-    for module in bnn.modules():
-        if hasattr(module, 'log_prior') and hasattr(module, 'log_posterior'):
-            kl_divergence += module.log_posterior - module.log_prior
-            if update_prior:
-                module.weight_prior.update(module.weight_distribution)
-                if module.bias:
-                    module.bias_prior.update(module.bias_distribution)
+def KL_divergence(bnn):
+    bnn_modules = [m for m in bnn.modules() if(hasattr(m, 'log_prior') and hasattr(m, 'log_posterior'))]
+    kl_divergence = bnn_modules[0].log_posterior - bnn_modules[0].log_prior
+    for module in bnn_modules[1:]:
+        kl_divergence += module.log_posterior - module.log_prior
     return kl_divergence
--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name='btorch',
-    version='0.5.5',
+    version='0.5.6',
     packages=find_packages(),
 )
