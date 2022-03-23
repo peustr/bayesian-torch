5a299a56e3062a601410534ac0ab9930f5a7f262
Panagiotis Eustratiadis <peustratiadis@gmail.com>
2022-03-23 11:53:08 +0000

    support for batchnorm layers

--- btorch/bnn/__init__.py
+++ btorch/bnn/__init__.py
@@ -1,2 +1,3 @@
-from .conv import Conv1d, Conv2d, ConvTranspose1d, ConvTranspose2d # noqa
+from .batchnorm import BatchNorm1d, BatchNorm2d  # noqa
+from .conv import Conv1d, Conv2d, ConvTranspose1d, ConvTranspose2d  # noqa
 from .linear import Linear  # noqa
