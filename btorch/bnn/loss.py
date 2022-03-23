0680bb8c1e867ecb69c55ffa8639b4c5562c3451
Panagiotis Eustratiadis <peustratiadis@gmail.com>
2022-03-23 11:48:38 +0000

    kl divergence supports new layers

--- btorch/bnn/loss.py
+++ btorch/bnn/loss.py
@@ -2,10 +2,20 @@ def kl_divergence(bnn, reduction='mean'):
     assert reduction in ('sum', 'mean')
     n, loss = 0., 0.
     for m in bnn.modules():
-        if hasattr(m, 'weight_posterior') and hasattr(m, 'weight_prior'):
+        if (
+            hasattr(m, 'weight_posterior') and
+            hasattr(m, 'weight_prior') and
+            m.weight_posterior is not None and
+            m.weight_prior is not None
+        ):
             n += 1.
             loss += _kl(m.weight_posterior, m.weight_prior)
-            if m.bias:
+            if (
+                hasattr(m, 'bias_posterior') and
+                hasattr(m, 'bias_prior') and
+                m.bias_posterior is not None and
+                m.bias_prior is not None
+            ):
                 loss += _kl(m.bias_posterior, m.bias_prior)
     if reduction == 'mean':
         loss /= n
